c3097a52b65743f895e813f2de95ec1b5eff6108
yuanchiam <ychiam@netflix.com>
2017-05-03 15:42:46 -0700

    Update quarterly_goals_metrics.sql

--- quarterly_goals_metrics.sql
+++ quarterly_goals_metrics.sql
@@ -80,3 +80,34 @@ and application_id='padme'
 and node_id not in (-12345)
 group by 1) sess
 
+
+-- Searches per Ticket
+-- those who perform at least one search per ticket 
+
+select avg(search.num_searches)
+from
+(select 
+kb.ticket_code,
+sum(c.kb_search_cnt) num_searches
+from dse.cs_contact_f c 
+join dse.geo_country_d geo on c.contact_origin_country_code = geo.country_iso_code 
+join dse.cs_transfer_type_d trt on c.transfer_type_id = trt.transfer_type_id 
+join dse.cs_contact_subchannel_d sub on c.contact_subchannel_id=sub.contact_subchannel_id 
+join dse.cs_contact_skill_d sk on c.contact_skill_id=sk.contact_skill_id 
+join dse.cs_kb_node_visit_f kb on c.first_ticket_id=kb.ticket_code
+left join (select * from dse.cs_recontact_f 
+     where days_to_recontact_cnt<=7 
+     and fact_utc_date >= 20170401 ) rcr on c.contact_code = rcr.contact_code
+where c.fact_utc_date between 20170401 and 20170630
+and sk.escalation_code = 'No-Escalation' 
+and trt.major_transfer_type_desc!='TRANSFER_OUT' 
+and sub.contact_channel_id in ('Phone','Chat') 
+and c.call_center_id in ('TPPL','ARMX','NCSL','24FR','24TB','24DV','24ML','TTBR','TLLV','SMBN','SMNL','TCLV','NCJP','TDSG')
+and c.answered_cnt>0 
+and c.cust_msg_cnt>=2 
+and kb.page_load_cnt>0
+and kb.application_id in ('padme','csinternalkb')
+and kb.article_id>0
+and kb.fact_date between 20170401 and 20170630
+and c.kb_search_cnt>0
+group by kb.ticket_code) search
