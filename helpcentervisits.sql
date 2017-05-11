a30f6cc82880e892b9c0434434f9eb4ae5e13717
yuanchiam <ychiam@netflix.com>
2017-05-11 12:51:00 -0700

    Update helpcentervisits.sql

--- helpcentervisits.sql
+++ helpcentervisits.sql
@@ -8,16 +8,19 @@ from
 fact_date,
 api.article_id,
 api.article_name, 
+kb.language_id,
+
 case when kb.language_id is null then api.language_id 
-     when lower(kb.language_id) in ('es-es','fr-ca','pt-pt','zh-tw','zh-cn') then substring(lower(kb.language_id),1,2)
-     else kb.language_id end as language_tmp,
+     when kb.language_id in ('es-es','fr-ca','pt-pt','zh-tw','zh-cn') then substr(lower(kb.language_id),1,2) else kb.language_id end as language_tmp,
+
 sum(coalesce(kb.visitors,0)) visitors
 
 from
 
 -- joining dse.cs_kb_article_d to find article names
 (select node.article_id, art.article_name, node.node_id, node.application_id,
-lower(node.language_id) as language_id
+case when lower(node.language_id) in ('es-es','fr-ca','pt-pt','zh-tw','zh-cn') then substr(lower(node.language_id),1,2)
+else lower(node.language_id) end as language_id
 from dse.cs_kb_node_d node
 left join dse.cs_kb_article_d art on node.article_id=art.article_id
 where node.application_id='padme' and node.is_published=1) api
@@ -34,7 +37,7 @@ where fact_date between cast(date_format(date_add('day',-95,current_date), '%Y%m
 and application_id='padme' group by 1,2,3,4) kb
 
 on kb.node_id=api.node_id and kb.application_id=api.application_id
-group by 1,2,3,4) kb1
+group by 1,2,3,4,5) kb1
 
 left join
 
@@ -78,4 +81,3 @@ and application_id='padme' group by 1,2,3,4
 
 on kb.node_id=api.node_id and kb.application_id=api.application_id
 group by 1,2,3,4
-
